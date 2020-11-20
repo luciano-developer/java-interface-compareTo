39cc1dd4f3545639617c8937b713358f1d3c94da
luciano-developer <lcoliveiraa@gmail.com>
2020-11-19 23:06:50 -0300

    interface compareTo in list

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,31 +2,35 @@ package application;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import entities.Employee;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		List<String> list = new ArrayList<String>();
+		List<Employee> list = new ArrayList<Employee>();
 		String path = "C:\\Users\\minha\\workspace-java-completo\\in.txt";
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(path))){
 			String line = br.readLine();
 			while (line != null) {
-				list.add(line);
+				String[] fields = line.split(",");
+				list.add(new Employee(fields[0], Double.parseDouble(fields[1])));
 				line = br.readLine();
 			}
 			
 			Collections.sort(list);
-			for (String string : list) {
-			System.out.println(string);	
+			for (Employee e : list) {
+			System.out.println(e.getName() + " - " + e.getSalary());	
 			}
 			 
-		} catch (Exception e) {
+		} catch (IOException e) {
 			// TODO: handle exception
 		}
 	}
